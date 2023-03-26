81bb533ba778384b5e4571b10c7c4fcacb893019
sbipr <smbipr@gmail.com>
2023-03-26 17:48:40 +0100

    Connection to instance script

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-# import requests
-# import json
-import worldometer
+import psycopg2
+import sys
+import boto3
+import os
+from dotenv.main import load_dotenv
+from pprint import pprint
 
+load_dotenv()
 
-if __name__ == '__main__':
-    print(worldometer.api.births_today())
+rds = boto3.client('rds',region_name='eu-west-2')
+
+ENDPOINT="database-1.cok8qfd47nby.eu-west-2.rds.amazonaws.com"
+PORT="5432"
+USER="postgres"
+REGION="eu-west-2"
+DBNAME="postgres"
+PASSWORD= os.environ['TOKEN']
+
+try:
+    conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASSWORD, sslrootcert="SSLCERTIFICATE")
+    cur = conn.cursor()
+    cur.execute("""SELECT now()""")
+    query_results = cur.fetchall()
+    pprint(query_results)
+except Exception as e:
+    pprint("Database connection failed due to {}".format(e))                
